068c4ec850fd755cb20d3ea1e5dbeeb9e185dd48
Franklin <zwemmendeeendjes@hotmail.com>
2014-04-21 22:32:04 +0000

    Added employee bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Lsw\MemcacheBundle\LswMemcacheBundle(),
+            new Employee\EmployeeBundle\EmployeeBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
